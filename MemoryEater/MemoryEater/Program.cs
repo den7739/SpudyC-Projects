23bb41b48e8aeea2e872fbdb70aeac5c38f3e9cb
den7739 <den7839den@gmail.com>
2016-07-21 20:36:04 +0300

    Project MemoryEater was added

---
+++ MemoryEater/MemoryEater/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemoryEater
+{
+    public class Limit
+    {
+        int limit;
+
+        public Limit(int memory)
+        {
+            limit = memory;
+        }
+
+        public bool IsLimit()
+        {
+            if (limit < GC.GetTotalMemory(false))
+                return true;
+            return false;
+        }
+
+        public void Error(object mes)
+        {
+            if (IsLimit())
+                Console.WriteLine("{0}", mes);
+        }
+    }
+
+    public class MemoryEater
+    {
+        int[] array = new int[1000000];
+
+        public void Method(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryEater[] mas = new MemoryEater[1000];
+            var timer = new Timer(new Limit(1000).Error, "Lost memory", 0, 200);
+
+            // var thread = new Thread(new Limit().Error("Error"));
+            for (int i = 0; i < mas.Length; i++)
+            {
+                new MemoryEater().Method(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
